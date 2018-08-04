1f637426d8cddd09be1df850226f7c6839a74b4e
druid & pyrcel <druid.pyrcel@gmail.com>
2018-08-05 00:35:27 +0200

    Adding professore on splash screen

--- app/src/main/java/com/labessence/biotech/finaldilution/peripherals/view/SplashActivity.kt
+++ app/src/main/java/com/labessence/biotech/finaldilution/peripherals/view/SplashActivity.kt
@@ -3,6 +3,8 @@ package com.labessence.biotech.finaldilution.peripherals.view
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import com.labessence.biotech.finaldilution.R
 
 /**
  * Created by dawid.chmielewski on 12/26/2016.
@@ -12,8 +14,14 @@ class SplashActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val intent = Intent(this, StartupActivity::class.java)
-        startActivity(intent)
-        finish()
+        setContentView(R.layout.activity_splash)
+
+        val waitHandler = Handler()
+
+        waitHandler.postDelayed({
+            val intent = Intent(this, StartupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
     }
 }
