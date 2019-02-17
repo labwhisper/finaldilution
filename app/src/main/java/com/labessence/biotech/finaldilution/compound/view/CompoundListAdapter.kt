3ef6b861b7c86fa297896ecf65b3c9f2ef85b032
druid & pyrcel <druid.pyrcel@gmail.com>
2019-02-17 13:30:11 +0100

    Missing files

---
+++ app/src/main/java/com/labessence/biotech/finaldilution/compound/view/CompoundListAdapter.kt
@@ -0,0 +1,44 @@
+package com.labessence.biotech.finaldilution.compound.view
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.labessence.biotech.finaldilution.R
+import com.labessence.biotech.finaldilution.compound.Compound
+
+class CompoundListAdapter(compounds: List<Compound>) :
+    RecyclerView.Adapter<CompoundListAdapter.CompoundViewHolder>() {
+
+    val compoundList: List<Compound> = compounds
+
+    class CompoundViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var nameView: TextView? = null
+        var massView: TextView? = null
+
+        init {
+            nameView = itemView.findViewById(R.id.compound_list_item_name_text_view)
+            massView = itemView.findViewById(R.id.compound_list_item_mass_text_view)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CompoundViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.compound_list_item, parent, false)
+        return CompoundViewHolder(itemView)
+    }
+
+    override fun getItemCount() = compoundList.size
+
+    override fun onBindViewHolder(holder: CompoundViewHolder, position: Int) {
+        val compound = compoundList.get(position)
+        holder.nameView?.text = compound.displayName
+        holder.massView?.text = holder.massView?.context?.resources?.getString(
+            R.string.mass_in_brackets,
+            compound.molarMass.toString()
+        )
+    }
+
+}
